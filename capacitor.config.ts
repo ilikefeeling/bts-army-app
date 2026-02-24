4da9adb9ea0fe5324ff2ef08ac4a293bce97ca82
신현구 <ilikefeeling@gmail.com>
2026-02-24 19:56:41 +0900

    chore: add iOS capacitor wrapper and github actions

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.btsarmy.app',
+  appName: 'BTS ARMY APP',
+  webDir: 'out'
+};
+
+export default config;
